f961595ee856e7cd49ed6d5bca32aed5857939a6
yakovlev <ay@skb.ru>
2020-09-29 09:54:37 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import TodoList from './Todo/TodoList';
+import Context from './context'
+
+function App() {
+    const [todos, setTodos] = React.useState([
+        {id:1, completed: false, title:'Купить хлеб'},
+        {id:2, completed: false, title:'Купить масло'},
+        {id:3, completed: false, title:'Купить молоко'}
+    ])
+
+    function toggleTodo(id){
+        setTodos(
+            todos.map(todo => {
+            if(todo.id===id){
+                todo.completed = !todo.completed
+            }
+            return todo
+        }))
+    }
+
+    function removeTodo(id) {
+        setTodos(todos.filter(todo=>todo.id!==id))
+    }
+
+  return (
+      <Context.Provider value={{removeTodo}}>
+      <div className='wrapper'>
+        <h1>Rect tutorial</h1>
+        <TodoList todos={todos} onToggle={toggleTodo} />
+      </div>
+      </Context.Provider>
+  )
+}
+
+export default App;
